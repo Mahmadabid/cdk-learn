ccc4cbb83e32debdc7638689f2120e8db4f50680
Mahmedabid <75790323+Mahmedabid@users.noreply.github.com>
2021-10-24 11:32:31 +0500

    created backend

--- 03amplify/Backend/lambda/mian.ts
+++ 03amplify/Backend/lambda/mian.ts
@@ -1,10 +1,30 @@
+import addTodo from "./addTodo";
+import delTodo from "./delTodo";
+import getTodos from "./getTodos";
 import Todo from "./Todo";
+import updateTodo from "./updateTodo";
 
 type AppSyncEvent = {
     info: {
-        fieldName: String
+        fieldName: string
     },
     arguments: {
+        todoId: string,
         todo: Todo
+    }
+}
+
+exports.handler = async(event: AppSyncEvent) => {
+    switch(event.info.fieldName) {
+        case "addTodo":
+            return await addTodo(event.arguments.todo);
+        case "delTodo":
+            return await delTodo(event.arguments.todoId);
+        case "updateTodo":
+            return await updateTodo(event.arguments.todo);
+        case "getTodos":
+            return await getTodos();
+        default:
+            return null;
     }
 }
--- 03amplify/Backend/lambda/updateTodo.ts
+++ 03amplify/Backend/lambda/updateTodo.ts
@@ -1,10 +1,47 @@
 import client from './ddbClient';
 
-async function updateTodo(todosId: string) {
-    const params = {
-        TableName: process.env.TODOS_TABLE? process.env.TODOS_TABLE: '',
-        key: {
-            id: todosId
+type Params = {
+    TableName: string,
+    Key: {},
+    ExpressionAttributeValues: any,
+    ExpressionAttributeNames: any,
+    UpdateExpression: string,
+    ReturnValues: string
+}
+
+async function updateTodo(todo: any) {
+
+    let params: Params = {
+        TableName: process.env.TODOS_TABLE ? process.env.TODOS_TABLE : '',
+        Key: {
+            id: todo.id
+        },
+        ExpressionAttributeNames: {},
+        ExpressionAttributeValues: {},
+        UpdateExpression: '',
+        ReturnValues: "UPDATED_NEW"
+    }
+
+    let prefix = 'set';
+    let attributes = Object.keys(todo);
+    for (var i = 0; i > attributes.length; i++) {
+        let attribute = attributes[i];
+        if (attribute !== 'id') {
+            params["UpdateExpression"] += prefix + "#" + attribute + " = :" + attribute;
+            params["ExpressionAttributeValues"][":" + attribute] = todo[attribute];
+            params["ExpressionAttributeNames"]["#" + attribute] = attribute;
+            prefix = ", ";
         }
     }
+
+    try {
+        await client.update(params).promise()
+        return todo
+    }
+    catch (err) {
+        console.log('error updating todos: ', err);
+        return null
+    }
 }
+
+export default updateTodo;
